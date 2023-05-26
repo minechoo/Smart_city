cf2c747767f26e5c5160d6cb436afa3fa3f2692d
추홍재 <chuhongjae@gmail.com>
2023-05-26 19:59:10 +0900

    modify deviceList

--- src/view/dashboard/DashBoard.vue
+++ src/view/dashboard/DashBoard.vue
@@ -33,9 +33,23 @@ export default {
             , { areaId: "area002", deviceName: "강남역 4번 출구 쉘터", deviceId: "device002", deviceType: "POLL" }
             , { areaId: "area003", deviceName: "강남역 4번 출구 스마트도서관", deviceId: "device003", deviceType: "LIBRARY" }
             , { areaId: "area004", deviceName: "역삼역 1번 출구 스마트폴", deviceId: "device004", deviceType: "STATION" }
-            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device005", deviceType: "POLL" }
+          
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0051", deviceType: "POLL" }
+
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0052", deviceType: "POLL" }
+
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0053", deviceType: "POLL" }
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0054", deviceType: "POLL" }
             , { areaId: "area006", deviceName: "도봉역 3번 출구 스마트폴", deviceId: "device006", deviceType: "LIBRARY" }
-            , { areaId: "area007", deviceName: "금정역 4번 출구 스마트폴", deviceId: "device007", deviceType: "STATION" }]
+            , { areaId: "area007", deviceName: "금정역 4번 출구 스마트폴", deviceId: "device007", deviceType: "STATION" }
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0055", deviceType: "POLL" }
+
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0056", deviceType: "POLL" }
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0057", deviceType: "POLL" }
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0058", deviceType: "POLL" }
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0059", deviceType: "POLL" }
+            , { areaId: "area005", deviceName: "삼성역 2번 출구 스마트폴", deviceId: "device0060", deviceType: "POLL" }
+        ]
 
         this.setDeviceList({ deviceList: sampleDeviceList });
 
