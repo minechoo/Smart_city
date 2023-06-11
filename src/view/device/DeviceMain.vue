75363edf9681f77483caf90b0ef1657a08dd4855
추홍재 <chuhongjae@gmail.com>
2023-06-11 17:13:19 +0900

    add main ui transition;

--- src/view/device/DeviceMain.vue
+++ src/view/device/DeviceMain.vue
@@ -1,49 +1,49 @@
 <template lang="">
-  <div class="menu_box p_40_140">
-    <div class="menu_box_inner">
-      <DeviceMainItem
-        v-for="device in list"
-        v-bind:key="device.id"
-        :stat="device.stat"
-        :name="device.moduleNm"
-        :type="device.moduleCd"
-        :deviceId="device.deviceId"
-        :moduleId="device.moduleId"
-        :start="device.start"
-        :end="device.end"
-        @showModifyDialog="showModifyDialog"
-        @fnReflesh="fnSearchModuleList"
-        @showDetails="(moduleId) => fnShowDetail(moduleId)"
-      />
-      <div class="go add" @click="showAddDialog">
-        <button>추가하기</button>
-      </div>
-    </div>
-  </div>
   <transition>
-    <Teleport to="#app">
-      <div class="dialog-dim" v-if="showDeviceAdd">
-        <DeviceModuleAdd @onClose="fnAddClosed" :deviceId="deviceId" />
-      </div>
-    </Teleport>
-  </transition>
-  <transition>
-    <Teleport to="#app">
-      <div class="dialog-dim" v-if="isShowDetail">
-        <DeviceDetailDialog
-          @onClose="fnHideDetail"
-          :deviceId="deviceId"
-          :moduleId="selectedModuleId"
+    <div class="menu_box p_40_140">
+      <div class="menu_box_inner">
+        <DeviceMainItem
+          v-for="device in list"
+          v-bind:key="device.id"
+          :stat="device.stat"
+          :name="device.moduleNm"
+          :type="device.moduleCd"
+          :deviceId="device.deviceId"
+          :moduleId="device.moduleId"
+          :start="device.start"
+          :end="device.end"
+          @showModifyDialog="showModifyDialog"
+          @fnReflesh="fnSearchModuleList"
+          @showDetails="(moduleId) => fnShowDetail(moduleId)"
         />
+        <div class="go add" @click="showAddDialog">
+          <button>추가하기</button>
+        </div>
       </div>
-    </Teleport>
-  </transition>
-  <transition>
-    <Teleport to="#app">
-      <div class="dialog-dim" v-if="isShowModify">
-        <ModuleModify :module="selectModuleObj" @onClose="hideModify" />
-      </div>
-    </Teleport>
+
+      <Teleport to="#app">
+        <transition>
+          <div class="dialog-dim" v-if="showDeviceAdd">
+            <DeviceModuleAdd @onClose="fnAddClosed" :deviceId="deviceId" />
+          </div>
+        </transition>
+
+        <transition>
+          <div class="dialog-dim" v-if="isShowDetail">
+            <DeviceDetailDialog
+              @onClose="fnHideDetail"
+              :deviceId="deviceId"
+              :moduleId="selectedModuleId"
+            />
+          </div>
+        </transition>
+        <transition>
+          <div class="dialog-dim" v-if="isShowModify">
+            <ModuleModify :module="selectModuleObj" @onClose="hideModify" />
+          </div>
+        </transition>
+      </Teleport>
+    </div>
   </transition>
 </template>
 <script>
@@ -66,7 +66,7 @@ export default {
       isShowDetail: false,
       isShowModify: false,
       selectedModuleId: "",
-      selectModuleObj :{},
+      selectModuleObj: {},
       list: [],
     };
   },
@@ -84,19 +84,18 @@ export default {
     },
   },
   mounted() {
-
-    // 화면 로드 이후 모듈 정보 조회 
+    // 화면 로드 이후 모듈 정보 조회
     this.fnSearchModuleList();
   },
   methods: {
     ...mapActions(["connectSocket", "getStatus"]),
 
-    // 기기추가 팝업 닫기 
+    // 기기추가 팝업 닫기
     fnAddClosed() {
       this.showDeviceAdd = false;
       this.fnSearchModuleList();
     },
-    // 기기 추가 팝업 표시 
+    // 기기 추가 팝업 표시
     showAddDialog() {
       this.showDeviceAdd = true;
     },
@@ -104,33 +103,31 @@ export default {
     showModify() {
       this.isShowModify = true;
     },
-    // 모듈 수정 팝업 닫기 
+    // 모듈 수정 팝업 닫기
     hideModify() {
       this.isShowModify = false;
       this.fnSearchModuleList();
     },
-    // 모듈 정보 수정 팝업 
+    // 모듈 정보 수정 팝업
     showModifyDialog(moduleId) {
-     
       this.isShowModify = true;
       this.selectedModuleId = moduleId;
-      this.selectModuleObj =  this.list.find(v => v.moduleId === this.selectedModuleId);
-
-
+      this.selectModuleObj = this.list.find(
+        (v) => v.moduleId === this.selectedModuleId
+      );
     },
     fnShowDetail(moduleId) {
-     
       this.selectedModuleId = moduleId;
       this.isShowDetail = true;
     },
     fnHideDetail() {
-      
       this.isShowDetail = false;
       this.fnSearchModuleList();
     },
 
-    // 모듈 정보 조회 
+    // 모듈 정보 조회
     async fnSearchModuleList() {
+      this.list = [];
       this.connectSocket(this.$route.params.deviceId);
 
       const { data } = await ComApi.post("/api/device/module/list", {
@@ -139,7 +136,7 @@ export default {
 
       this.list = data;
 
-      // 모듈 정보 조회 이후 기기상태 vuex 전문 요청 호출 
+      // 모듈 정보 조회 이후 기기상태 vuex 전문 요청 호출
       this.getStatus({
         userId: this.getUserInfo.userId,
         deviceId: this.deviceId,
